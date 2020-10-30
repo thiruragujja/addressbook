266d80b008a9fdc331f3b6534e92b14cc97be35a
root <root@ubuntu.nxtsandbox.local>
2020-10-30 15:06:22 +0800

    comment

---
+++ src/test/java/com/edurekademo/utilities/MyTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class MyTest {
+ @Test
+ public void onePlusOneShouldBeTwo() {
+ int sum = 1 + 1;
+ assertEquals(2, sum);
+ }
+}
